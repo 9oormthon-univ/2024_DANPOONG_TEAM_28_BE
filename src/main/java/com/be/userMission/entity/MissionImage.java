d017b61b649bcf106e2c903acdd41efa1df9a97e
진미나 <mina200238@gmail.com>
2024-11-24 01:37:09 +0900

    Feat: add mission image

--- src/main/java/com/be/userMission/entity/MissionImage.java
+++ src/main/java/com/be/userMission/entity/MissionImage.java
@@ -1,2 +1,31 @@
-package com.be.userMission.entity;public class MissionImage {
+package com.be.userMission.entity;
+
+import com.be.mission.entity.Mission;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "mission_image")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class MissionImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id", nullable = false)
+    private Mission mission;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @Builder
+    public MissionImage(Mission mission, String url) {
+        this.mission = mission;
+        this.url = url;
+    }
 }
